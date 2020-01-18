9561c78d202340993e83d816fefee1361e181919
Ken Hasegawa <te10ma@hotmail.co.jp>
2020-01-19 02:38:05 +0900

    ・LocalAuthenticationで認証する様に対応

--- SuicaReader/ViewController.swift
+++ SuicaReader/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import LocalAuthentication
+
 import NFCReader
 
 class ViewController: UIViewController {
@@ -16,10 +18,39 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.authenticate()
     }
 }
 
-private extension ViewController {    
+private extension ViewController {
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        let description: String = "認証"
+        
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            let errorDescription = error?.description ?? "Face ID / Touch IDが利用できません"
+            let alertController = UIAlertController(title: "", message: errorDescription, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .cancel))
+            self.present(alertController, animated: true)
+            return
+        }
+        
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: description) { [unowned self] (success, error) in
+            guard error == nil else {
+                return
+            }
+            
+            let alertController = UIAlertController(title: "",
+            message: success ? "認証に成功しました" : "認証に失敗しました",
+            preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "閉じる", style: .cancel))
+            DispatchQueue.main.sync {
+                self.present(alertController, animated: true)
+            }
+        }
+    }
+    
     func read() {
         suicaReader.read(didDetect: { [unowned self]  reader, result in
             switch result {
